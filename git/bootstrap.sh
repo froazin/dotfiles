12b5ee0a09866a623ce62037253f65d65b810819
Froazin <40861351+Froazin@users.noreply.github.com>
2023-10-06 01:04:47 +0100

    fix: stop overwritting git config in devcontainers

--- git/bootstrap.sh
+++ git/bootstrap.sh
@@ -4,7 +4,7 @@ source .modules/logging.sh 2> /dev/null       || exit 1
 source .modules/common.sh 2> /dev/null        || exit 1
 source .modules/devcontainers.sh 2> /dev/null || exit 1
 
-check_requirements git rsync                  || exit 1
+check_requirements git                        || exit 1
 
 warnings=false
 
@@ -15,28 +15,31 @@ if check_devcontainer; then
     warnings=true
 fi
 
-write_log $INFO "Synchronizing git config."
-rsync --exclude install.sh \
-    --exclude bootstrap.sh \
-    -avz --no-perms ./git/ $HOME > /dev/null 2>&1 || exit 1
+cp git/.gitignore_global ~/.gitignore_global && \
+    write_log $DEBUG "Copied git/.gitignore_global to ~/.gitignore_global" || \
+    write_log $ERROR "Failed to copy git/.gitignore_global to ~/.gitignore_global"
 
 if $devcontainer; then
-    exit 2
-fi
+    write_log $DEBUG "Will not modify git global config because the script is running in a devcontainer."
+else
+    username=$(git config --global user.name)
+    email=$(git config --global user.email)
 
-username=$(git config --global user.name)
-email=$(git config --global user.email)
+    cp git/.gitconfig ~/.gitconfig && \
+        write_log $DEBUG "Copied git/.gitconfig to ~/.gitconfig" || \
+        write_log $ERROR "Failed to copy git/.gitconfig to ~/.gitconfig"
 
-if ! [ -z "$username" ]; then
-    write_log $DEBUG "Existing git configuration for global user.name <$username> was found."
-    write_log $INFO "Persisting existing git configuration for global user.name"
-    git config --global user.name "$username" || exit 1
-fi
+    if ! [ -z "$username" ]; then
+        write_log $DEBUG "Existing git configuration for global user.name <$username> was found."
+        write_log $INFO "Persisting existing git configuration for global user.name"
+        git config --global user.name "$username" || exit 1
+    fi
 
-if ! [ -z "$email" ]; then
-    write_log $DEBUG "Existing git configuration for global user.email <$email> was found."
-    write_log $INFO "Persisting existing git configuration for global user.email"
-    git config --global user.email "$email" || exit 1
+    if ! [ -z "$email" ]; then
+        write_log $DEBUG "Existing git configuration for global user.email <$email> was found."
+        write_log $INFO "Persisting existing git configuration for global user.email"
+        git config --global user.email "$email" || exit 1
+    fi
 fi
 
 if $warnings; then
